8f5932c817b1af82998cde244b37e1b48d6b833a
Ariel Coppes <ariel.coppes@gemserk.com>
2012-11-06 18:04:27 -0200

    added option to switch between normal sprite batch and polygon sprite batch

--- libgdx-test-apps-core/src/main/java/com/gemserk/libgdx/tests/PolygonSpriteBatchTestScreen.java
+++ libgdx-test-apps-core/src/main/java/com/gemserk/libgdx/tests/PolygonSpriteBatchTestScreen.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.PolygonSprite;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -25,11 +27,15 @@ public class PolygonSpriteBatchTestScreen extends TestScreen {
 	TextureAtlas atlas;
 	Skin skin;
 	Stage stage;
+
 	PolygonSpriteBatch polygonSpriteBatch;
+	SpriteBatch spriteBatch;
 
 	ArrayList<PolygonSprite> polygonSprites;
+	ArrayList<Sprite> sprites;
 
 	boolean blending;
+	boolean usePolygonSpriteBatch;
 
 	@Override
 	public void create() {
@@ -43,22 +49,42 @@ public class PolygonSpriteBatchTestScreen extends TestScreen {
 
 		atlas = new TextureAtlas(Gdx.files.internal("data/images/polygon.atlas"));
 
-		final PolygonRegion polygonRegion = new PolygonRegion(atlas.createSprite("worm"), Gdx.files.internal("data/polygons/worm"));
+		final Sprite wormSprite = atlas.createSprite("worm");
+		final PolygonRegion polygonRegion = new PolygonRegion(wormSprite, Gdx.files.internal("data/polygons/worm"));
 
 		polygonSprites = new ArrayList<PolygonSprite>();
+		sprites = new ArrayList<Sprite>();
 
 		polygonSpriteBatch = new PolygonSpriteBatch();
+		spriteBatch = new SpriteBatch();
 
 		stage = new Stage(width, height, false);
 
 		blending = true;
+		usePolygonSpriteBatch = true;
 
-		generateSprites(polygonRegion, 100);
+		generatePolygonSprites(wormSprite, polygonRegion, 100);
 
 		Table optionsContainer = new Table();
 		optionsContainer.setTransform(false);
 
 		{
+			{
+				final CheckBox actor = new CheckBox("Use polygons: " + usePolygonSpriteBatch, skin);
+				actor.setName("Blending");
+				actor.addListener(new ClickListener() {
+					@Override
+					public void clicked(InputEvent event, float x, float y) {
+						super.clicked(event, x, y);
+						usePolygonSpriteBatch = !usePolygonSpriteBatch;
+						actor.setText("Use polygons: " + usePolygonSpriteBatch);
+					}
+				});
+				optionsContainer.add(actor).padLeft(10f).padRight(10f).expandX().fillX().padBottom(10f).colspan(5);
+			}
+
+			optionsContainer.row();
+
 			{
 				final CheckBox actor = new CheckBox("Blending: " + blending, skin);
 				actor.setName("Blending");
@@ -90,7 +116,7 @@ public class PolygonSpriteBatchTestScreen extends TestScreen {
 					@Override
 					public void clicked(InputEvent event, float x, float y) {
 						super.clicked(event, x, y);
-						removeSprites(50);
+						removePolygonSprites(50);
 					}
 				});
 				optionsContainer.add(actor).padLeft(10f).padRight(10f).expandX().fillX().padBottom(10f);
@@ -102,7 +128,7 @@ public class PolygonSpriteBatchTestScreen extends TestScreen {
 					@Override
 					public void clicked(InputEvent event, float x, float y) {
 						super.clicked(event, x, y);
-						generateSprites(polygonRegion, 50);
+						generatePolygonSprites(wormSprite, polygonRegion, 50);
 					}
 				});
 				optionsContainer.add(actor).padLeft(10f).padRight(10f).expandX().fillX().padBottom(10f);
@@ -127,22 +153,37 @@ public class PolygonSpriteBatchTestScreen extends TestScreen {
 		Gdx.input.setCatchBackKey(true);
 	}
 
-	private void generateSprites(PolygonRegion polygonRegion, int count) {
+	private void generatePolygonSprites(Sprite sprite, PolygonRegion polygonRegion, int count) {
+
 		for (int i = 0; i < count; i++) {
 			PolygonSprite polygonSprite = new PolygonSprite(polygonRegion);
 			polygonSprite.setPosition(MathUtils.random(0, Gdx.graphics.getWidth()) - polygonSprite.getWidth() * 0.5f, MathUtils.random(0f, Gdx.graphics.getHeight()) - polygonSprite.getHeight() * 0.5f);
 			polygonSprites.add(polygonSprite);
 		}
+
+		for (int i = 0; i < count; i++) {
+			Sprite wormSprite = new Sprite(sprite);
+			wormSprite.setPosition(MathUtils.random(0, Gdx.graphics.getWidth()) - wormSprite.getWidth() * 0.5f, MathUtils.random(0f, Gdx.graphics.getHeight()) - wormSprite.getHeight() * 0.5f);
+			sprites.add(wormSprite);
+		}
+
 		TextButton button = (TextButton) stage.getRoot().findActor("SpritesCount");
 		if (button != null)
 			button.setText("Sprites: " + polygonSprites.size());
 	}
 
-	private void removeSprites(int count) {
+	private void removePolygonSprites(int count) {
+
 		while (!polygonSprites.isEmpty() && count > 0) {
 			polygonSprites.remove(0);
 			count--;
 		}
+
+		while (!sprites.isEmpty() && count > 0) {
+			sprites.remove(0);
+			count--;
+		}
+
 		TextButton button = (TextButton) stage.getRoot().findActor("SpritesCount");
 		if (button != null)
 			button.setText("Sprites: " + polygonSprites.size());
@@ -162,21 +203,35 @@ public class PolygonSpriteBatchTestScreen extends TestScreen {
 		polygonSpriteBatch = null;
 		polygonSprites.clear();
 		polygonSprites = null;
+		spriteBatch.dispose();
+		spriteBatch = null;
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		if (blending)
-			polygonSpriteBatch.enableBlending();
-		else
-			polygonSpriteBatch.disableBlending();
+		if (usePolygonSpriteBatch) {
+			if (blending)
+				polygonSpriteBatch.enableBlending();
+			else
+				polygonSpriteBatch.disableBlending();
 
-		polygonSpriteBatch.begin();
-		for (int i = 0; i < polygonSprites.size(); i++)
-			polygonSprites.get(i).draw(polygonSpriteBatch);
-		polygonSpriteBatch.end();
+			polygonSpriteBatch.begin();
+			for (int i = 0; i < polygonSprites.size(); i++)
+				polygonSprites.get(i).draw(polygonSpriteBatch);
+			polygonSpriteBatch.end();
+		} else {
+			if (blending)
+				spriteBatch.enableBlending();
+			else
+				spriteBatch.disableBlending();
+
+			spriteBatch.begin();
+			for (int i = 0; i < sprites.size(); i++)
+				sprites.get(i).draw(spriteBatch);
+			spriteBatch.end();
+		}
 
 		stage.draw();
 	}
